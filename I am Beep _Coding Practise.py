0c703423cd134094b81b9237a819b35ac0990f88
Omobulejo Olabisi <149266563+OlabisiOmobulejo@users.noreply.github.com>
2023-10-28 21:30:18 +0100

    Add files via upload

---
+++ I am Beep _Coding Practise.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# "simple message"
+
+# In[1]:
+
+
+"simple message"
+
+
+# In[2]:
+
+
+# Display message to the standard output 
+
+print("system failure imminent!")
+
+
+# "multiline_message"
+
+# In[3]:
+
+
+# Display message to the standard output 
+
+print("system failure imminent!")
+print ("system reboot has been initiated...")
+
+
+# In[4]:
+
+
+# Display message to the Multiline_message 
+
+print("system failure imminent!")
+print ("system reboot has been initiated...")
+
+print("")
+
+print("...rebooting sensoring system")
+print("...rebooting output motors")
+print("...rebooting ourput engine")
+
+print("")
+
+print("system online")
+
+
+# " Escape_Characters"
+
+# In[5]:
+
+
+# Display output to Escape_Characters
+
+print("\n displays a new line")
+print("\t displays a tab space")
+print("\\ displays a back slash")
+print("\" displays a double quote")
+print("\'displays a single quote")
+
+
+# In[18]:
+
+
+print("\n displays a new line")
+print("\t displays a tab space")
+print("\\ displays a back slash")
+print("\" displays a double quote")
+print("\'displays a single quote")
+
+print("")
+print("")
+print("")
+
+print("\t")
+
+print('"I am Beep!"')
+
+
+# In[ ]:
+
+
+
+
